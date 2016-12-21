effaf076d2362ed789b4ca721ba1503f5620af55
Harpinoth <mazsparrow@gmail.com>
2016-12-21 10:22:33 +0100

    furthermore
     - work, work

--- zmiennefuruta.m
+++ zmiennefuruta.m
@@ -1,11 +1,11 @@
 M=1;
-Il=1;
-l=1;
+Il=0.03;
+l=0.3;
 ml=1;
-Ir=1;
-r=1;
-g=1;
-k=1;
+Ir=0.083;
+r=0.5;
+g=9.81;
+k=1000;
 
 detD = M*(Il*l + ml*l*l)*(Ir*r + M*r*r + ml*l*l);
 a1 = (1/detD)*((-M*(M*r*l)+ml*l*r)*(-M*g*l-ml*g*l*0.5));
@@ -17,6 +17,18 @@ a6 = (1/detD)*((Ir*r+M*r*r+ml*l*l)*2*l*M*g+(-2*M*M*r*r*l-2*M*r*r*ml*l)*M*g+(-M*M
 b1 = (1/detD)*(M*(Il*l+l*l+ml*l*l));
 b2 = (1/detD)*(-M*(M*r*l+ml*l*r));
 D=0;
-C=[0;0;1;0;0;0];
+C=[0 0 1 0 0 0];
 B=[0;b1;0;b2;0;0];
 A = [0 1 0 0 0 0;0 0 a1 0 a4 0;0 0 0 1 0 0; 0 0 a2 0 a5 0;0 0 0 0 0 1;0 0 a3 0 a6 0];
+furuta = ss(A,B,C,D);
+t = 0:0.1:100;
+u = [0*t];
+x0 = [0 0 0 0 10 0];
+figure(1)
+lsim(furuta,u,t,x0)
+
+K = place (A,B, [-0.9 -0.8 -0.7 -0.6 -0.5 -0.4]);
+ster = ss(A-B*K,[],C,0);
+figure(2)
+[Y,T,x]=lsim(ster,u,t,x0);
+plot(T,Y)
